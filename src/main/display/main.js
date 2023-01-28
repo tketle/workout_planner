5891614e9214552d4301eb9ab3571b81837aee18
Collin Baldwin <collin.baldwin1@gmail.com>
2023-01-28 06:50:29 -0500

    Add plus button to muscle group tables

--- src/main/display/main.js
+++ src/main/display/main.js
@@ -13,7 +13,6 @@ function render_container_table() {
     render_muscle_group_content("#pull_container", muscle_groups_list[1]);
     render_muscle_group_content("#core_container", muscle_groups_list[2]);
     render_muscle_group_content("#legs_container", muscle_groups_list[3]);
-    init_tooltips();
 }
 
 function render_muscle_group_content(selector, muscle_group) {
@@ -28,31 +27,26 @@ function render_muscle_group_content(selector, muscle_group) {
 
 function construct_muscle_region_table(muscle_region) {
     var muscle_region_name = muscle_region["name"];
+    var muscle_region_name_treated = replace_whitespace_and_special_characters(muscle_region_name);
 
     var $muscle_region_table = $("<table/>", {
-        id: replace_whitespace_and_special_characters(muscle_region_name) + "_table",
-        class: "table table-light"
+        id: muscle_region_name_treated + "_table",
+        class: "table table-light table-striped"
     });
 
-    var $header = $("<thead/>", {
-        "html": $("<tr/>", {
-            "html": $("<th/>").html(muscle_region_name),
-            class: "table-success"
-        })
-    });
+    $muscle_region_table.append(construct_muscle_region_header(muscle_region_name));
+    $muscle_region_table.append(create_exercise_col_group());
 
-    $muscle_region_table.append($header);
+    var $table_body = $("<tbody/>").addClass("table-group-divider").css("border-top-color", "#999999");
 
-    var $table_body = $("<tbody/>", {
-        "html": $("<tr/>", {
-            "html": $("<td/>", {
-                "html": construct_exercises_table(
-                    replace_whitespace_and_special_characters(muscle_region_name),
-                        muscle_region["exercises"])
-            })
-        }),
-        class: "table-group-divider",
-        style: "border-top-color: #999999"
+    muscle_region["exercises"].forEach(function(exercise) {
+        var $row = $("<tr/>");
+        $row.append("<td>" + exercise.name + "</td>");
+        $row.append("<td>" + exercise.targeted_muscles.join(", ") + "</td>");
+        $row.append("<td>" + exercise.equipment + "</td>");
+        $row.append($("<td/>", {html: create_fa_icon("pen", "fa-sm")}));
+        $row.append($("<td/>", {html: create_fa_icon("trash", "fa-sm")}));
+        $table_body.append($row);
     });
 
     $muscle_region_table.append($table_body);
@@ -60,30 +54,23 @@ function construct_muscle_region_table(muscle_region) {
     return $muscle_region_table;
 }
 
-function construct_exercises_table(muscle_region_name, exercise_list) {
-    var $exercise_table = $("<table/>", {
-        id: muscle_region_name + "_exercises_table",
-        class: "table table-striped",
-        style: "border-color: #999999"
-    });
+function construct_muscle_region_header(muscle_region_name) {
+    var $header_row = $("<tr/>", {class: "table-success"});
 
-    $exercise_table.append(create_exercise_col_group());
+    $header_row.append($("<th/>", {
+        html: muscle_region_name,
+        scope: "col"
+    }));
 
-    var $table_body = $("<tbody/>");
+    $header_row.append($("<th/>", {
+        html: create_fa_icon("square-plus", "fa-xl"),
+        scope: "col",
+        colspan: 4,
+        class: "text-end",
+        style: "padding-right: 15px;"
+    }));
 
-    exercise_list.forEach(function(exercise) {
-        var $row = $("<tr/>");
-        $row.append("<td>" + exercise.name + "</td>");
-        $row.append("<td>" + exercise.targeted_muscles.join(", ") + "</td>");
-        $row.append("<td>" + exercise.equipment + "</td>");
-        $row.append($("<td/>", {html: create_edit_icon()}));
-        $row.append($("<td/>", {html: create_trash_icon()}));
-        $table_body.append($row);
-    });
-
-    $exercise_table.append($table_body);
-
-    return $exercise_table;
+    return $("<thead/>").html($header_row);
 }
 
 function create_exercise_col_group() {
@@ -97,17 +84,9 @@ function create_exercise_col_group() {
     return $col_group;
 }
 
-function create_edit_icon() {
-    return create_icon("fa-pen");
-}
-
-function create_trash_icon() {
-    return create_icon("fa-trash");
-}
-
-function create_icon(icon) {
+function create_fa_icon(icon, size) {
     return $("<i/>", {
-                class: "fa-solid fa-sm icon " + icon
+                class: "fa-solid " + size + " icon fa-" + icon
             });
 }
 
